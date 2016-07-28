7913d146fe1d354f165cabcae8798fc77d65b7e8
Brandon Bassett <bassett.brandon@gmail.com>
2016-07-27 22:28:52 -0600

    add routes

--- web/GET.ex
+++ web/GET.ex
@@ -4,7 +4,9 @@ defmodule EncodeBot.Resource.GET do
   mediatype Mazurka.Mediatype.Hyperjson do
     action do
       %{
-        "greeting" => Greeting.world()
+        "greeting" => Greeting.world(),
+        "encode" => link_to("POST /encode"),
+        "decode" => link_to("POST /decode")
       }
     end
   end
---
+++ web/encode/POST.ex
@@ -0,0 +1,26 @@
+defmodule EncodeBot.Resource.Encode.POST do
+  use Mazurka.Resource
+
+  let string = Input.get("string")
+
+  mediatype Mazurka.Mediatype.Hyperjson do
+    action do
+      %{
+        string
+        # |> Base.encode64()
+        # |> String.replace("=", "")
+      }
+    end
+
+    affordance do
+      %{
+        "input" => %{
+          "string" => %{
+            "type" => "text",
+            "required" => "true"
+          }
+        }
+      }
+    end
+  end
+end
